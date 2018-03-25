f910e8f4123be26f09a46815402aa0ae18a80d03
Gary Doublé <garydouble@live.com>
2018-03-25 02:18:16 +0100

    a few more tests and build fixes

---
+++ test/type.tests.js
@@ -0,0 +1,19 @@
+
+const expect = require('chai').expect
+const { Token, Node } = require('../dist/lexy')
+
+describe('types', () => {
+  it('Token', () => {
+    expect(new Token('value', 'type')).to.eql({
+      type: 'type',
+      value: 'value'
+    })
+  })
+  it('Node', () => {
+    expect(new Node('value', 'type')).to.eql({
+      type: 'type',
+      value: ['value'],
+      children: []
+    })
+  })
+})
