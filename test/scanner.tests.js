bf5c880afb8df8b48ba8774a92f60abe7822369c
Gary Doublé <garydouble@live.com>
2018-03-25 00:11:04 +0000

    refactoring, with some scanner tests

---
+++ test/scanner.tests.js
@@ -0,0 +1,53 @@
+
+const expect = require('chai').expect;
+const { Token, scanner } = require('../dist/lexy');
+
+describe('scanner', () => {
+  it('supports empty input', () => {
+    let scan = scanner({
+      vocabulary: [{ type: 'A', pattern: /a/ }]
+    });
+
+    expect(scan()).to.eql([]);
+    expect(scan('')).to.eql([]);
+  });
+  it('supports symbol flattening (flatten)', () => {
+    let scan = scanner({
+      vocabulary: [{ type: '#!$', pattern: /[\#\!\$]/, flatten: true }]
+    });
+
+    expect(scan('$#!')).to.eql([
+      new Token('$', '$'),
+      new Token('#', '#'),
+      new Token('!', '!')
+    ]);
+  });
+  it('supports "greedy" tokens', () => {
+    let scan = scanner({
+      vocabulary: [{ type: 'A', pattern: /a/ }]
+    });
+
+    expect(scan('aaa')).to.eql([
+      new Token('a', 'A'),
+      new Token('a', 'A'),
+      new Token('a', 'A')
+    ]);
+
+    scan = scanner({
+      vocabulary: [{ type: 'A', pattern: /a/, greedy: true }]
+    });
+
+    expect(scan('aaa')).to.eql([
+      new Token('aaa', 'A')
+    ]);
+  });
+  it('supports token transforms', () => {
+    let scan = scanner({
+      vocabulary: [{ type: 'NUM', pattern: /\d+/, transform: value => parseFloat(value) }]
+    });
+
+    expect(scan('123')).to.eql([
+      new Token(123.0, 'NUM')
+    ]);
+  })
+})
